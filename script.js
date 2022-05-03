4698bce1eca5e3b72b84bf8cbb032a6c9825f77e
alexnik197 <nikiforov197@gmail.com>
2022-05-03 14:45:14 +0700

    edit: refact code

--- script.js
+++ script.js
@@ -8,37 +8,48 @@ function getSecretNum () {
 let secretNum = getSecretNum ();
 let score = 20;
 let highScore = 0;
+function message (message) {
+    document.querySelector('#message').textContent = message;
+}
+
+function clear () {
+    document.querySelector('#btn-cont').classList.add('btn-hide');
+    document.querySelector('#btn-check').classList.remove('btn-hide');
+    document.querySelector('#input').value = '';
+    document.querySelector('body').style.backgroundColor = '#FCFCFC';
+    document.querySelector('#secret-box').style.width = '90px';
+    secretNum = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('#secret-num').textContent = '?';
+    message(`🧐 Type a number...`);
+}
 
 // Событие на кнопку
 document.querySelector('#btn-check').addEventListener('click', function (){
 let input = Number(document.querySelector('#input').value);
     if (!input || input > 20) {
-        document.querySelector('.main-message__text').textContent = '🛑 Between 1 and 20';
+        message('🛑 Between 1 and 20');
         } else if (score > 0) {
             // WRONG
-            if (input > secretNum) {
-                document.querySelector('.main-message__text').textContent = `📈 Too high!`;
+            if (input !== secretNum) {
+                let wrong = input > secretNum ? message('📈 Too high') : message('📉 Too low');
                 score--;
-                document.querySelector('.main-score__score-num').textContent = score;
-            } else if (input < secretNum) {
-                document.querySelector('.main-message__text').textContent = '📉 Too low!';
-                score--;
-                document.querySelector('.main-score__score-num').textContent = score;
+                document.querySelector('#score').textContent = score;
+                return wrong;
 
             // WIN
             } else if (input === secretNum) {
-                document.querySelector('.main-message__text').textContent = `😎 Correct number!`;
+                message(`😎 Correct number!`);
                 highScore++;
-                document.querySelector('.main-score__highscore-num').textContent = highScore;
-                document.querySelector('.main-check__secret-num').textContent = secretNum;
+                document.querySelector('#highscore').textContent = highScore;
+                document.querySelector('#secret-num').textContent = secretNum;
                 document.querySelector('body').style.backgroundColor = '#00c968';
-                document.querySelector('.main-check__secret').style.width = '180px';
+                document.querySelector('#secret-box').style.width = '180px';
                 document.querySelector('#btn-cont').classList.remove('btn-hide');
                 document.querySelector('#btn-check').classList.add('btn-hide');
             }
             // GAME OVER
         } else if (score < 1) {
-            document.querySelector('.main-message__text').textContent = '😞 Game over!';
+            message('😞 Game over!');
             document.querySelector('body').style.backgroundColor = '#a60024';
         };        
     });
@@ -46,29 +57,15 @@ let input = Number(document.querySelector('#input').value);
 
 // CONTINUE BUTTON
 document.querySelector('#btn-cont').addEventListener('click', function (){
-    document.querySelector('#btn-cont').classList.add('btn-hide');
-    document.querySelector('#btn-check').classList.remove('btn-hide');
-    document.querySelector('#input').value = '';
-    document.querySelector('body').style.backgroundColor = '#FCFCFC';
-    document.querySelector('.main-check__secret').style.width = '90px';
-    secretNum = Math.trunc(Math.random() * 20) + 1;
-    document.querySelector('.main-check__secret-num').textContent = '?';
-    document.querySelector('.main-message__text').textContent = `🧐 Type a number...`;
+    clear();
 });
 
 
 // RESET BUTTON
-document.querySelector('.main-message__btn-reset').addEventListener('click', function (){
-    document.querySelector('#btn-cont').classList.add('btn-hide');
-    document.querySelector('#btn-check').classList.remove('btn-hide');
-    document.querySelector('#input').value = '';
-    document.querySelector('body').style.backgroundColor = '#FCFCFC';
-    document.querySelector('.main-check__secret').style.width = '90px';
-    document.querySelector('.main-check__secret-num').textContent = '?';
-    secretNum = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('#btn-reset').addEventListener('click', function (){
+    clear();
     score = 20;
-    document.querySelector('.main-score__score-num').textContent = score;
     highScore = 0;
-    document.querySelector('.main-score__highscore-num').textContent = highScore;
-    document.querySelector('.main-message__text').textContent = `🧐 Type a number...`;
+    document.querySelector('#score').textContent = score;
+    document.querySelector('#highscore').textContent = highScore;
 });
